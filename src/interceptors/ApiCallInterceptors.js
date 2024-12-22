94d0e569f73ad3a5fd2c8f82712f079f14f0a332
Soumyabrata Sarkar <sarkar.soumyabrata2@gmail.com>
2024-12-22 21:19:10 +0530

    Removed console logs

--- src/interceptors/ApiCallInterceptors.js
+++ src/interceptors/ApiCallInterceptors.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 const postApiCall = async (path, payload) => {
-  console.log(process.env);
-  console.log(process.env.REACT_APP_SERVICE_BASE_URL + path);
-
   return await axios.post(
     process.env.REACT_APP_SERVICE_BASE_URL + path,
     payload
